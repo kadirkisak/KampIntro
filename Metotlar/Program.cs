2357b44248ddbfba6822c703cb8bd425163390a6
Kadir Kısak <kadirkisak@gmail.com>
2021-01-17 17:45:10 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Kayısı";
+            urun2.Fiyati = 20;
+            urun2.Aciklama = "Malatya kayısısı";
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------------------------------");
+
+
+            }
+
+            Console.WriteLine("------------Metotlar-----------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+
+            sepetManager.Ekle2("Armut", "Deveci Armudu", 25, 10);
+            sepetManager.Ekle2("Çilek", "Silifke Sera", 32, 5);
+        }
+    }
+}
